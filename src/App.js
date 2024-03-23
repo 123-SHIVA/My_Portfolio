13a9bbcf79cffb5ce185a59ccc4a6d79aaf29d73
123-SHIVA <shivayojha8@gmail.com>
2024-03-24 01:21:43 +0530

    completed intro section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/navbar/Navbar";
-
+import Intro from "./components/Intro/Intro";
 function App() {
   return (
     <div className="App">
   <Navbar/>
+  <Intro/>
        
     </div>
   );
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './intro.css'
+import bg from '../../assets/assets/myimg.png'
+import btnImg from '../../assets/assets/hireme.png'
+import { Link } from 'react-scroll';
+
 
 const Intro = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='intro'>
+        <div className='introContent'>
+            <span className='hello'>Hello,</span>
+            <span className='introText'>I'm <span className='introName'>Shivam</span><br/>website desinger</span>
+            <p className='intropara'>I am a skilled web desiner with experience in creating <br/>visually appealing and user friendly websites.</p>
+            <Link ><button className='btn'><img src={btnImg} alt='hire me' className='btnImg'/>Hire Me</button></Link>
+        </div>
+        <img src={bg} alt='Profile' className='bg'/>
+
+    </section>
+   
   )
 }
 
