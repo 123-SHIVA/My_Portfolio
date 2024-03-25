7e5ae40f55fb826c5a84bff66fdb5ca05dc8ef22
123-SHIVA <shivayojha8@gmail.com>
2024-03-25 23:31:46 +0530

    Project section started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 import Skill from "./components/Skills/Skill";
+import Project from "./components/Project/Project";
 function App() {
   return (
     <div className="App">
   <Navbar/>
   <Intro/>
   <Skill/>
+  <Project/>
        
     </div>
   );
