24d1d819ff803ece438705a57c0d2e16b90c6c99
123-SHIVA <shivayojha8@gmail.com>
2024-04-12 00:45:14 +0530

    making some changes in project Section

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./project.css";
 import portfolio1 from "../../assets/assets/Indiabusiness.png";
 import portfolio2 from "../../assets/assets/beatsLanding.png";
-import portfolio3 from "../../assets/assets/portfolio-3.png";
+import portfolio3 from "../../assets/assets/Todo.png";
 import portfolio4 from "../../assets/assets/portfolio-4.png";
 import portfolio5 from "../../assets/assets/portfolio-5.png";
 import portfolio6 from "../../assets/assets/portfolio-6.png";
@@ -37,12 +37,38 @@ const Project = () => {
       {/* {data.map((item) => (
       ))} */}
         <div className="workImgs">
+        <div className="project-1">
         <a href="https://indiabusiness.org.in/"> <img src={portfolio1} alt="" className="worksImg" /></a> 
+        <div className="project-1desc">
+         <h2>ITC-ONLINE WEB APP</h2>
+
+         <p>A web-based tool helping users manage finances with expense tracking, budgeting features, and visualizations. It offers secure authentication and data encryption for privacy. Empowers users to make informed financial decisions.</p>
+
+         <button>visit</button>
+
+        </div>
+
+        </div>
+        <div className="project-1">
+
+        <div className="project-1desc">
+         <h2>ITC-ONLINE WEB APP</h2>
+
+         <p>A web-based tool helping users manage finances with expense tracking, budgeting features, and visualizations. It offers secure authentication and data encryption for privacy. Empowers users to make informed financial decisions.</p>
+
+         <button>visit</button>
+
+        </div>
         <a href="https://123-shiva.github.io/Beats-landing-page/"> <img src={portfolio2} alt="" className="worksImg" /></a>
-          <img src={portfolio3} alt="" className="worksImg" />
-          <img src={portfolio4} alt="" className="worksImg" />
+     
+
+        </div>
+        
+    
+        <a href="https://123-shiva.github.io/ToDo-list-/">   <img src={portfolio3} alt="" className="worksImg" /></a>
+          {/* <img src={portfolio4} alt="" className="worksImg" />
           <img src={portfolio5} alt="" className="worksImg" />
-          <img src={portfolio6} alt="" className="worksImg" />
+          <img src={portfolio6} alt="" className="worksImg" /> */}
         </div>
         <button className="worksBtn">See More</button>
     </section>
